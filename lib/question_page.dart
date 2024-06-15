3f567281478acaa936c7c840bcd338d565c0a81d
Efe Can Tepe <59337173+efecantepe@users.noreply.github.com>
2024-06-15 18:56:25 +0300

    Sonraki soru özelliği çalısmıyor

--- lib/question_page.dart
+++ lib/question_page.dart
@@ -5,67 +5,46 @@ import 'package:flutter/material.dart';
 import 'package:general_knowlodge/app_colors.dart';
 import 'package:general_knowlodge/helper.dart';
 
-
-Stream<String> periodicQuestionStream(String input, Duration period) async*{
-  for(int i = 0; i < input.length; i++){
+Stream<String> periodicQuestionStream(String input, Duration period) async* {
+  for (int i = 0; i < input.length; i++) {
     yield input[i];
     await Future.delayed(period);
   }
 }
 
 List<Map<String, dynamic>> questions = [
-
   {
-
-    "question" : "Istanbulu Kim Fethetti?",
-    "choices" : <String> [
-
+    "question": "Istanbulu Kim Fethetti?",
+    "choices": <String>[
       "Yıldırım Beyazid",
       "Fatih Sultan Mehmed",
       "Kanuni Sultan Süleyman",
       "2. Abdulhamid"
-
     ],
-
-    "answer" : "Fatih Sultan Mehmed"
+    "answer": "Fatih Sultan Mehmed"
   },
-
   {
-
-    "question" : "Türkiye Cumhuriyetini Kim Kurdu?",
-
-     "choices" : <String> [
-
+    "question": "Türkiye Cumhuriyetini Kim Kurdu?",
+    "choices": <String>[
       "Bülent Ecevit",
       "Efe Can Tepe",
       "Mustafa Kemal Atatürk",
       "Celal Bayar"
-
     ],
-
-    "answer" : "Mustafa Kemal Atatürk"
-
+    "answer": "Mustafa Kemal Atatürk"
   },
-
   {
-    "question" : "Hangisi Bir Avrupa Devleti Değildir?",
-
-     "choices" : <String> [
-      "Fransa",
-      "Almanya",
-      "İspanya",
-      "Fas"
-    ],
-
-    "answer" : "Fas"
-
+    "question": "Hangisi Bir Avrupa Devleti Değildir?",
+    "choices": <String>["Fransa", "Almanya", "İspanya", "Fas"],
+    "answer": "Fas"
   },
-
 ];
 
-class QuestionPage extends StatefulWidget{
+class QuestionPage extends StatefulWidget {
 
-  
+  int currentQuestionIndex = 0;
+
+
   QuestionPage({super.key});
 
   @override
@@ -73,37 +52,43 @@ class QuestionPage extends StatefulWidget{
 }
 
 class _QuestionPageState extends State<QuestionPage> {
-  
-  void handleAnswer(String answer){
 
-    print("Answer is: ${answer}");
-
-
-
+  void goToNextQuestion(){
+    currentQuestionIndex++;
+    setChoices();
   }
 
+  void handleAnswer(String answer) {
+
+    setState(() {
+      isAnswerClicked = true;
+    });
+
+    
+
+    print("Answer is: ${answer}");
+  }
 
   Future<void> setChoices() async {
-
+    
     Completer<void> questionCompleter = Completer<void>();
 
-    questionSubscription = questionStream.listen((char) { 
-        setState(() {
-          question += char;
-        });
-    },
-
-      onDone: () {
-        questionCompleter.complete();
-      } 
-    );
+    questionSubscription = questionStream.listen((char) {
+      setState(() {
+        question += char;
+      });
+    }, onDone: () {
+      questionCompleter.complete();
+    });
 
     await questionCompleter.future;
 
-    for (int a = 0; a < (questions[0]["choices"] as List<String>).length; a++) {
+    for (int a = 0; a < (questions[currentQuestionIndex]["choices"] as List<String>).length; a++) {
       Completer<void> completer = Completer<void>();
-      Stream<String> choiceStream = periodicQuestionStream((questions[0]["choices"] as List<String>)[a], Duration(milliseconds: 30));
-      
+      Stream<String> choiceStream = periodicQuestionStream(
+          (questions[currentQuestionIndex]["choices"] as List<String>)[a],
+          Duration(milliseconds: 30));
+
       choiceSub = choiceStream.listen((char) {
         setState(() {
           choices[a] += char;
@@ -116,28 +101,29 @@ class _QuestionPageState extends State<QuestionPage> {
     }
   }
 
-  int currentQuestionIndex = 0;
   late bool isChoiceEnabled = false;
-  final Stream<String> questionStream = periodicQuestionStream(questions[0]["question"], Duration(milliseconds: 50));
-  List<String> choices = (questions[0]["choices"] as List<String>).map((e) => "").toList();
+  final Stream<String> questionStream = periodicQuestionStream(
+      questions[0]["question"], Duration(milliseconds: 50));
+  List<String> choices =
+      (questions[0]["choices"] as List<String>).map((e) => "").toList();
 
   late StreamSubscription<String> questionSubscription;
   late StreamSubscription<String> choiceSub;
+  late int currentQuestionIndex;
   String question = "";
+
+  bool isAnswerClicked = false;
 
 
   @override
   void initState() {
-
     super.initState();
-    
+    currentQuestionIndex = widget.currentQuestionIndex;
     setChoices();
   }
 
-
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     int questionNumber = currentQuestionIndex + 1;
     //String question = questions[currentQuestionIndex]["question"]!;
 
@@ -145,132 +131,121 @@ class _QuestionPageState extends State<QuestionPage> {
     final double width = MediaQuery.sizeOf(context).width;
 
     return Scaffold(
-
       backgroundColor: AppColors.loginStartColor,
-
       appBar: AppBar(
-
         title: Text("Soru ${questionNumber.toString()}"),
         backgroundColor: AppColors.loginStartColor,
-
       ),
-
       body: Padding(
-        padding:  const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 64.0),
+        padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 64.0),
         child: SizedBox(
-        
           width: double.infinity,
-
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 question,
-                style: const TextStyle(
-                  fontSize: 32
-                ),
+                style: const TextStyle(fontSize: 32),
               ),
-
-              const SizedBox(height: 10,),
-
+              const SizedBox(
+                height: 10,
+              ),
               Column(
                 children: [
-                  for(String choice in choices) 
-                    choiceButtonWidget(choice),
+                  for (String choice in choices) choiceButtonWidget(choice),
                 ],
               ),
-
-              SizedBox(height: height / 20,),
-              
+              SizedBox(
+                height: height / 20,
+              ),
               Spacer(),
-
               Center(
                 child: SizedBox(
-
                   width: double.infinity,
                   height: height / 10,
-                  
-                  child: ElevatedButton(
-                    onPressed: () {
-
-                      if(!questionSubscription.isPaused){
-                        questionSubscription.pause();
-                      }
-
-                      else{
-                        questionSubscription.resume();
-                      }
-
-                      if(!choiceSub.isPaused){
-                        choiceSub.pause();
-                      }
-
-                      else{
-                        choiceSub.resume();
-                      }
-
-                      setState(() {
-                        isChoiceEnabled = !isChoiceEnabled;
-                      });
-
-                    }, 
-
-                    style: ElevatedButton.styleFrom(
-
-                      shape: RoundedRectangleBorder(
-
-                        borderRadius: BorderRadius.circular(Helper.CHOICE_BORDER_RADIUS)
-
-                      )
-                    ),
-
-                    child: const Text("BAS", 
-                      style: TextStyle(
-                        fontSize: 42,
-                        fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ),
+                  child: clickButtonWidget(isAnswerClicked)
                 ),
               )
             ],
           ),
         ),
       ),
-
     );
-
   }
 
   Widget choiceButtonWidget(String choice) {
     return Column(
       children: [
-        ElevatedButton( 
-                        onPressed: isChoiceEnabled ? () => handleAnswer(choice) : null,
-                        style: ElevatedButton.styleFrom(
-                          disabledBackgroundColor: AppColors.choiceBackgroundDisenabled,
-                          backgroundColor: AppColors.choiceBackgroundEnabled  ,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(Helper.CHOICE_BORDER_RADIUS)
-                          )
-                        ), 
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            
-                            choice,textAlign: 
-                            TextAlign.left,
-                            style: TextStyle(
-
-                              color: Colors.black
-
-                            ), 
-                          )
-                        ),
-
-                      ),
-        const SizedBox(height: 10,)
+        ElevatedButton(
+          onPressed: isChoiceEnabled ? () => handleAnswer(choice) : null,
+          style: ElevatedButton.styleFrom(
+              disabledBackgroundColor: AppColors.choiceBackgroundDisenabled,
+              backgroundColor: AppColors.choiceBackgroundEnabled,
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(Helper.CHOICE_BORDER_RADIUS))),
+          child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                choice,
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.black),
+              )),
+        ),
+        const SizedBox(
+          height: 10,
+        )
       ],
     );
   }
+
+  Widget clickButtonWidget(bool isAnswerClicked){
+    if(isAnswerClicked){
+      return ElevatedButton(
+                      onPressed: () {
+                        
+                        goToNextQuestion();
+
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  Helper.CHOICE_BORDER_RADIUS))),
+                      child: const Text(
+                        "Sonraki Soru",
+                        style: TextStyle(
+                            fontSize: 42, fontWeight: FontWeight.bold),
+                      ));
+  
+    }
+
+    return ElevatedButton(
+                      onPressed: () {
+                        if (!questionSubscription.isPaused) {
+                          questionSubscription.pause();
+                        } else {
+                          questionSubscription.resume();
+                        }
+
+                        if (!choiceSub.isPaused) {
+                          choiceSub.pause();
+                        } else {
+                          choiceSub.resume();
+                        }
+
+                        setState(() {
+                          isChoiceEnabled = !isChoiceEnabled;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  Helper.CHOICE_BORDER_RADIUS))),
+                      child: const Text(
+                        "BAS",
+                        style: TextStyle(
+                            fontSize: 42, fontWeight: FontWeight.bold),
+                      ));
+  }
+
 }
